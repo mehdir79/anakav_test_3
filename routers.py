7e87b7fee0a46cf73487987af715bbfd2d11eeec
mohammad mehdi rashidi <mehdirashidi530@gmail.com>
2025-08-01 14:55:27 +0330

    salam

--- routers.py
+++ routers.py
@@ -87,6 +87,104 @@ def get_tests():
             i += 1
         df = pd.DataFrame.from_dict(testsdict, orient="index")
         return df.to_json(orient="table")
+    
+@router.post("tests/add_test")
+def add_test(test_name : str , test_num : int, test_majmo : str ):
+    with session.begin() as con:
+        try:
+            n_test = tests(name= test_name , test_num= test_num , majmo_name=test_majmo)
+            con.add(n_test)
+            return {"massage": "test added successfully"}
+        except:
+            return HTTPException(400 , "something went wrong maybe test_num already exists of the test_name is duplicate")
+
+
+@router.put("tests/edit_test")
+def edit_test(test_name : Optional[str] , test_num : Optional[int], new_test_name :Optional[str], new_test_num : Optional[int] ,new_test_majmo : Optional[str]):
+    with session.begin() as con:
+        try:
+            if test_name and test_num:
+                test = con.query(tests).where(and_(tests.test_name == test_name , tests.test_num == test_num)).first()
+                if test:
+                    if new_test_majmo or new_test_name or new_test_num:
+                        if new_test_num:
+                            test.test_num = new_test_num
+                        if new_test_name:
+                            test.test_name = new_test_name
+                        if new_test_majmo:
+                            test.majmo_name = new_test_majmo
+                        con.commit()
+                        return {"massage" : "test edited successfully"}
+                    else:
+                        return {"massage" : "you should enter at least one parameter to change"}
+                return {"massage" : "test does not exist"}
+            elif test_name:
+                test = con.query(tests).where(tests.test_name == test_name).first()
+                if test:
+                    if new_test_majmo or new_test_name or new_test_num:
+                        if new_test_num:
+                            test.test_num = new_test_num
+                        if new_test_name:
+                            test.test_name = new_test_name
+                        if new_test_majmo:
+                            test.majmo_name = new_test_majmo
+                        con.commit()
+                        return {"massage" : "test edited successfully"}
+                    else:
+                        return {"massage" : "you should enter at least one parameter to change"}
+                return {"massage" : "test does not exist"}
+            elif test_num:
+                test = con.query(tests).where(tests.test_num == test_num).first()
+                if test:
+                    if new_test_majmo or new_test_name or new_test_num:
+                        if new_test_num:
+                            test.test_num = new_test_num
+                        if new_test_name:
+                            test.test_name = new_test_name
+                        if new_test_majmo:
+                            test.majmo_name = new_test_majmo
+                        con.commit()
+                        return {"massage" : "test edited successfully"}
+                    else:
+                        return {"massage" : "you should enter at least one parameter to change"}
+                return {"massage" : "test does not exist"}
+            else:
+                return {"massage" : "enter test_name or test_num"}
+        except:
+            return HTTPException(400 , "something went wrong maybe test_num already exists of the test_name is duplicate")
+
+@router.delete("tests/delete_test")
+def delete_test(test_name : Optional[str] , test_num : Optional[int]):
+    with session.begin() as con:
+        try:
+            if test_name and test_num:
+                test = con.query(tests).where(and_(tests.test_name == test_name , tests.test_num == test_num).first())
+                if test:
+                    con.delete(test)
+                    return{"massage" : "test deleted successfully"}
+                else:
+                    return{"massage" : "test not found"}
+            elif test_name:
+                test = con.query(tests).where(tests.test_name == test_name).first()
+                if test:
+                    con.delete(test)
+                    return{"massage" : "test deleted successfully"}
+                else:
+                    return{"massage" : "test not found"}
+            elif test_num:
+                test = con.query(tests).where(tests.test_num == test_num).first()
+                if test:
+                    con.delete(test)
+                    return{"massage" : "test deleted successfully"}
+                else:
+                    return{"massage" : "test not found"}
+            else:
+                return{"massage" : "enter test_name or test_num"}
+        except:
+            return HTTPException(400 , "something went wrong")
+
+    
+    
 
 @router.get("/get_parameters")
 def get_parameters():
@@ -103,6 +201,49 @@ def get_parameters():
         df = pd.DataFrame.from_dict(parametersdict, orient="index")
         return df.to_json(orient="table")
 
+@router.post("/add_parameter")
+def add_parameter(parameter_name : str):
+    with session.begin() as con:
+        try:
+            if_par = con.query(parameters).where(parameters.parameter_name == parameter_name)
+            if not if_par:
+                n_parameter = parameters(parameter_name=parameter_name)
+                con.add(n_parameter)
+                return{"massage" : "parameter added successfully"}
+            else:
+                return{"massage" : "parameter exists"}
+        except:
+            return HTTPException(400 , "something went wrong")
+
+@router.put("/edit_parameter")
+def edit_parameter(parameter_name : str , new_parameter_name : str):
+    with session.begin() as con:
+        try:
+            par = con.query(parameters).where(parameters.parameter_name == parameter_name).first()
+            if par:
+                par.parameter_name = new_parameter_name
+                con.commit()
+                return{"massage" : "parameter edited successfully"}
+            else:
+                return{"massage" : "parameter does not exist"}
+        except:
+            return HTTPException(400 , "something went wrong")
+
+@router.delete("/delete_parameter")
+def delete_parameter(parameter_name : str):
+    with session.begin() as con:
+        try:
+            par = con.query(parameters).where(parameters.parameter_name == parameter_name).first()
+            if par:
+                con.delete(par)
+                return{"massage" : "parameter deleted successfully"}
+            else:
+                return{"massage" : "parameter does not exist"}
+        except:
+            return HTTPException(400 , "something went wrong")
+
+
+
 @router.get("/get_time_perids")
 def get_time_periods():
     with session.begin() as con:
@@ -117,6 +258,40 @@ def get_time_periods():
             i += 1
         df = pd.DataFrame.from_dict(time_periodsdict, orient="index")
         return df.to_json(orient="table")
+
+@router.post("/add_time_perids")
+def add_time_periods(year : int, month : int):
+    with session.begin() as con:
+        try:
+            time_period_query = con.query(time_priod).where(and_(time_priod.month == month , time_priod.year == year)).first()
+            if not time_period_query:
+                n_tp = time_priod(year = year , month= month)
+                return{"massage":"time period added successfully"}
+            else:
+                return {"maasage" : "time period exists"}
+        except:
+            return HTTPException(400,"something went wrong")
+
+
+@router.delete("/delete_time_perids")
+def delete_time_periods(year : int, month : int , new_year :int, new_month:int):
+    with session.begin() as con:
+        try:
+            time_period_query = con.query(time_priod).where(and_(time_priod.month == month , time_priod.year == year)).first()
+            time_period_query1 = con.query(time_priod).where(and_(time_priod.month == new_month , time_priod.year == new_year)).first()
+            if time_period_query :
+                if not time_period_query1:
+                    time_period_query.month = new_month
+                    time_period_query.year = new_year
+                    return{"massage":"time period editted successfully"}
+                else:
+                    return{"massage" : "the new time period exists try deleting the current"}
+            else:
+                return {"maasage" : "time period does not exist"}
+        except:
+            return HTTPException(400,"something went wrong")
+
+
 
 
 @router.get(
